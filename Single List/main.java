43e931fa12ef8ed46e4c501390cf7702ff9cc19c
shjoo0407 <69780999+shjoo0407@users.noreply.github.com>
2023-01-09 23:32:28 +0900

    Add files via upload

---
+++ Single List/main.java
@@ -0,0 +1,44 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SList<String> s = new SList<String>();
+		s.insertFront("orange"); s.insertFront("apple");
+		s.insertAfter("cherry", s.head.getNext());
+		s.insertFront("pear");
+		s.print();
+		System.out.println(":s의 길이 = "+s.size()+"\n");
+		
+		System.out.println("체리가 \t"+s.search("cherry")+"번째에 있다.");
+		System.out.println("키위가 \t"+s.search("kiwi")+"번째에 있다.");
+		System.out.println();
+		
+		s.deleteAfter(s.head);s.print();
+		System.out.println(":s의 길이 = "+s.size()+"\n");
+		
+		s.deleteFront();s.print();
+		System.out.println(":s의 길이 = "+s.size()+"\n");
+		
+		s.deleteFront();
+		s.deleteFront();
+		s.print();
+		System.out.println(":s의 길이 = "+s.size()+"\n");
+
+		SList<Integer> t = new SList<Integer>();
+		t.insertFront(500);
+		t.insertFront(200);
+		t.insertAfter(400, t.head);
+		t.insertFront(100);
+		t.insertAfter(300, t.head.getNext());
+		t.print();
+		System.out.println(":t의 길이 = "+t.size()+"\n");
+		
+
+
+		
+		
+		
+	}
+
+}
